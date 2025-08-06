8726d1aecad3b9b1e649d88083b05e323358d805
Walace Oliveira <walacer72@gmail.com>
2025-08-06 04:32:18 -0300

    acerto tamanho area aboult

--- src/components/aboult.tsx
+++ src/components/aboult.tsx
@@ -5,9 +5,9 @@ export const Aboult = () => {
 
     return (
         <div id='Aboult' className="min-h-screen w-screen flex justify-center items-center">
-            <div className="flex flex-col gap-4 md:gap-8 justify-center items-center w-full max-w-6xl p-2 md:p-8 mx-4">
+            <div className="flex flex-col gap-4 md:gap-8 justify-center items-center w-full max-w-6xl p-4 md:p-8 mx-4">
                 <h1 className="bg-gradient-to-r from-sky-800 to-sky-200 bg-clip-text text-4xl font-semibold text-transparent">Aboult</h1>
-                <div className="flex flex-col md:gap-8 p-8 bg-[rgba(0,0,0,0.5)] rounded-3xl">
+                <div className="flex flex-col md:gap-8 p-4 md:p-8 bg-[rgba(0,0,0,0.5)] rounded-3xl">
                     <p className="text-sm text-slate-500 md:leading-relaxed">Sou um profissional em transição de carreira para a área de tecnologia, atualmente cursando Análise e Desenvolvimento de Sistemas. Apesar de ainda não ter experiência formal na área de desenvolvimento, venho me preparando intensamente através de cursos práticos com foco em desenvolvimento web.
 
                         Tenho facilidade para aprender novas tecnologias e conceitos — ao longo da minha trajetória sempre me destaquei pela velocidade com que absorvo e aplico novos conhecimentos.
@@ -34,7 +34,7 @@ export const Aboult = () => {
                                 <p className="text-sm text-slate-500 opacity-50">Projectos academicos</p>
                             </div>
                         </div>
-                        <button className="bg-sky-700 rounded-full transition-all ease-in px-6 hover:-translate-y-2">Curriculo</button>
+                        <button className="bg-sky-700 rounded-full inline-block transition-all ease-in px-6 hover:-translate-y-2">Curriculo</button>
                     </div>
                 </div>
             </div>
