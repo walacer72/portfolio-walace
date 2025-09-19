b4d3a618fe8242aabef9be5ce9877d22d95a9e4b
Walace Oliveira <walacer72@gmail.com>
2025-09-19 16:07:52 -0300

    cor em button selecionado em menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,37 +1,45 @@
 'use client'
 
-import { BiCodeAlt, BiMessageDetail, BiUserCircle } from 'react-icons/bi'
-import { AiFillHome, AiOutlineMail } from 'react-icons/ai'
+import { BiCodeAlt } from 'react-icons/bi'
+import { AiFillHome } from 'react-icons/ai'
 import { FaUser, FaTools, FaFolderOpen } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
-import { IoPersonCircleSharp } from 'react-icons/io5'
- 
+import { useState } from 'react'
+
 export const Menu = () => {
 
-    const lista = ['Home', 'About','Skills', 'Projects', 'Contact'];
+    const lista = ['Home', 'About', 'Skills', 'Projects', 'Contact'];
+    const [key, setKey] = useState(0);
+
+    const handleButtonShow = (index: number) => {
+        setKey(index);
+        
+    }
 
     return (
         <>
             <ul className='md:hidden lg:hidden py-2 px-8 rounded-full flex w-full gap-8 justify-evenly items-center text-primary'>
-                
-                {lista.map(item => (
+
+                {lista.map((item, index) => (
 
                     <li key={item} className='flex items-center cursor-pointer font-light transition-all duration-200 group sm:text-sm md:text-base xl:text-base'>
 
                         <a
-                            className='transition-all ease-in duration-200 hover:-translate-y-1'
+                            key={index}
+                            onClick={() => handleButtonShow(index)}
+                            className={`transition-all ease-in duration-200 hover:-translate-y-1 ${key === index ? 'text-sky-700': ''}`}
                             href={`#${item}`}
                         >
-                            {item === 'Home' && <AiFillHome className='w-6 h-6'/>}
-                            {item === 'About' && <FaUser  className='w-5 h-5'/>}
-                            {item === 'Skills' && <FaTools className='w-5 h-5'/>}
-                            {item === 'Projects' && <FaFolderOpen className='w-6 h-6'/>}
-                            {item === 'Contact' && <MdEmail className='w-6 h-6'/>}
+                            {item === 'Home' && <AiFillHome className='w-6 h-6' />}
+                            {item === 'About' && <FaUser className='w-5 h-5' />}
+                            {item === 'Skills' && <FaTools className='w-5 h-5' />}
+                            {item === 'Projects' && <FaFolderOpen className='w-6 h-6' />}
+                            {item === 'Contact' && <MdEmail className='w-6 h-6' />}
                         </a>
                     </li>
                 ))}
 
-            </ul>  
+            </ul>
             <ul className='hidden md:flex py-2 px-8 rounded-full w-full gap-8 justify-between items-center text-primary'>
                 <BiCodeAlt className='text-sky-700 text-2xl' />
                 {lista.map((item, index) => (
@@ -39,13 +47,15 @@ export const Menu = () => {
                     <li key={item} className='flex items-center cursor-pointer font-light transition-all duration-200 group sm:text-sm md:text-base xl:text-base'>
 
                         <a
-                            className='transition-all ease-in duration-200 hover:-translate-y-1'
+                            key={index}
+                            onClick={() => handleButtonShow(index)}
+                            className={`transition-all ease-in duration-200 hover:-translate-y-1 ${key === index ? 'text-sky-700': ''}`}
                             href={`#${item}`}
                         >
                             {item === 'Home' && 'Início'}
                             {item === 'About' && 'Sobre mim'}
                             {item === 'Skills' && 'Habilidades'}
-                            {item === 'Projects' && 'Projetos'} 
+                            {item === 'Projects' && 'Projetos'}
                             {item === 'Contact' && 'Contato'}
                         </a>
                     </li>
