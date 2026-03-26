53811023ed256326188b4171a9890d3ee19c3fd7
Walace Oliveira <walacer72@gmail.com>
2026-03-26 15:15:46 -0300

    update link github

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -6,7 +6,7 @@ export const Icons = () => {
             <a
                 rel="noopener noreferrer"
                 target='_blank'
-                href="https://github.com/walacer72">
+                href="https://github.com/walacer72?tab=repositories">
                 <BiLogoGithub className='w-8 h-8 md:h-10 md:w-10 cursor-pointer rounded-full border-transparent bg-sky-700 p-1 md:p-2 text-white transition-all duration-200 hover:scale-110 hover:border-sky-800 hover:bg-slate-300 hover:text-sky-700' />
             </a>
 
